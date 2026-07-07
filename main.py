e01b5715a45d777ffad94ae9a2c49cb813fa33a7
Johannes N Opperman <jn.opperman@gmail.com>
2026-07-07 17:57:15 +0200

    Implemented Robot movement simulation

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import sys
 
+# How do we turn
+LEFT = {
+    "N": "W",
+    "W": "S",
+    "S": "E",
+    "E": "N",
+}
+
+RIGHT = {
+    "N": "E",
+    "E": "S",
+    "S": "W",
+    "W": "N",
+}
+
+# How do we move 
+MOVE = {
+    "N": (0, 1),
+    "E": (1, 0),
+    "S": (0, -1),
+    "W": (-1, 0),
+}
 
 def parse_robot(position, instructions):
     x, y, direction = position.split()
@@ -16,6 +38,8 @@ def main():
 
     max_x, max_y = map(int, lines[0].split())
 
+    scents = set()
+
     robots = []
 
     for i in range(1, len(lines), 2):
@@ -25,7 +49,33 @@ def main():
     print()
 
     for robot in robots:
-        print(robot)
+        x = robot["x"]
+        y = robot["y"]
+        direction = robot["direction"]
+        instructions = robot["instructions"]
+
+        lost = False
+        for instruction in instructions:
+            if instruction == "L":
+                direction = LEFT[direction]
+            elif instruction == "R":
+                direction = RIGHT[direction]
+            elif instruction == "F":
+                dx, dy = MOVE[direction]
+                next_x = x + dx
+                next_y = y + dy
+
+                if 0 <= next_x <= max_x and 0 <= next_y <= max_y:
+                    x, y = next_x, next_y
+                else:
+                    scent = (x, y, direction)
+                    if scent in scents:
+                        continue
+
+                    scents.add(scent)
+                    lost = True
+                    break
+        print(f"{x} {y} {direction}{' LOST' if lost else ''}")
 
 
 if __name__ == "__main__":
